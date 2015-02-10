332299729a740949bba2b7893892b29344e3c27f
ellencurrin <ellen.c.currin@gmail.com>
2015-02-10 13:05:35 -0500

    fine tuning layer toggle

--- js/biomass.js
+++ js/biomass.js
@@ -8,12 +8,12 @@ var facilitiesLayer = 'biomass_data/facilities_pellet_all.geojson'
 
 
 $( document ).ready(function() {
-    loadLayers()
+    //loadLayers()
     buildMap();
 }); 
 
 
-function loadLayers () { 
+/*function loadLayers () { 
     $.ajax({
             type:"GET",
             url: 'biomass_data/facilities_pellet_all.geojson',
@@ -25,7 +25,7 @@ function loadLayers () {
 function parseData(data){
         var dataObj = $.parseJSON(data);
         console.log(dataObj);
-}
+}*/
 
 //****** BUILDING MAP 
 function buildMap() {
@@ -35,99 +35,148 @@ function buildMap() {
         .setView([33.6190, -84.7266], 6)
         
     
+    var layers = document.getElementById('layerList');
+    var color = '#fff'
+    
+    ///// ADDING FACILITIES
+    addLayer(omnivore.geojson('biomass_data/facilities_pellet_operating.geojson'), 'Operating', 2);
+    addLayer(omnivore.geojson('biomass_data/facilities_pellet_proposed.geojson'), 'Proposed', 1);
+    
+    function addLayer(layer, name, zIndex) {
+        layer
+            .setZIndex(zIndex)
+            .on('ready', function(go) {
+                this.eachLayer(function(marker) {
+                    
+                    //// SETTING TOOLTIP & LABEL CONTENTS
+                    var content = '<h2>'+marker.feature.properties.plant_name+'<\/h2>'
+                        + '<p>Status: ' + marker.feature.properties.status + '</br>'
+                        +'Nearest City: ' + marker.feature.properties.city_near + '</p>'
+                        + '<p>Notes: ' + marker.feature.properties.notes + '</br>'
+                        + 'Website: <a href = " '+ marker.feature.properties.website + ' " target="_blank">' + marker.feature.properties.website + '</a></p>' ;
+                    
+                    marker.bindPopup(marker.feature.properties.map_label, {closeButton: false});
+                                        
+                    marker.on('mouseover', function() {
+                        marker.openPopup();
+                    });
+
+                    
+                    //// ADDING PLANT NAMES TO DROPDOWN LIST
+                    
+                    plantList = document.getElementById('plant-list').appendChild(document.createElement('li'));
+                    plantList.className = 'droplist';
+                    plantList.innerHTML = marker.feature.properties.plant_name;                
+                    plantList.onclick = function() {
+                        marker.bindPopup(content);
+                        L.mapbox.tileLayer('elcurr.l4gdgnij').addTo(map);
+                        map.setView(marker.getLatLng(), 16);
+                        marker.openPopup();
+                    };
+                    
+                    //// MARKER CLICK EVENT
+                    marker.on('click', function(){
+                        marker.bindPopup(content);
+                        L.mapbox.tileLayer('elcurr.l4gdgnij').addTo(map);
+                        map.setView(marker.getLatLng(), 16);
+                        marker.openPopup();
+                        } );
+                    
+                    //// SETTING MARKER STYLES FOR OPERATING/PROPOSED
+                    if (marker.feature.properties.status === 'Operating') {
+                        // The argument to L.mapbox.marker.icon is based on the
+                        // simplestyle-spec: see that specification for a full
+                        // description of options.
+                        color= '#911815'
+                        marker.setIcon(L.mapbox.marker.icon({
+                            'marker-color': color,
+                            'marker-size': 'small'
+                        }));
+                    } else {
+                        color= '#E85118';
+                        marker.setIcon(L.mapbox.marker.icon({
+                            'marker-color': color,
+                            'marker-size': 'small'
+                        }));
+                    }
+                })
+            })
+            .addTo(map);
+    
+        // Create a simple layer switcher that
+        // toggles layers on and off.        
+        var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox'
+            //checkbox.className = 'active';
+            checkbox.innerHTML = name;
+            checkbox.checked = 'True';
+        var label = document.createElement('label');
+        var description = document.createTextNode(name);
+        
+        label.appendChild(checkbox);
+        label.appendChild(description);
+        layers.appendChild(label);
+            
+        checkbox.onclick = function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            
+            if (map.hasLayer(layer)) {
+                map.removeLayer(layer);
+                $(checkbox).prop('checked', false);
+            } else {
+                map.addLayer(layer);
+                $(checkbox).prop('checked', true);
+            }
+        };
+        
+    }
+        
+        
     //// ADDING PORTS
     ports = omnivore.geojson('biomass_data/ports_of_export.geojson')
-    .on('ready', function(go) {
-            this.eachLayer(function(marker) {
-                var content = '<h2>Port<\/h2>';
-                marker.bindPopup(content);
-                marker.setIcon(L.mapbox.marker.icon({
-                        'marker-color': '#052945',
-                        'marker-size': 'small',
-                        'marker-symbol': 'ferry'
-                    }));
-            })
-    })
-    .addTo(map);
-    
-    toggleLayers()
-}
-    
-    //// ADDING WOOD PELLET FACILITIES
-function addFacilities() { 
-    facilities = omnivore.geojson(facilitiesLayer)
         .on('ready', function(go) {
-            this.eachLayer(function(marker) {
-                
-                //// SETTING TOOLTIP & LABEL CONTENTS
-                var content = '<h2>'+marker.feature.properties.plant_name+'<\/h2>'
-                    + '<p>Status: ' + marker.feature.properties.status + '</br>'
-                    +'Nearest City: ' + marker.feature.properties.city_near + '</p>'
-                    + '<p>Notes: ' + marker.feature.properties.notes + '</br>'
-                    + 'Website: <a href = " '+ marker.feature.properties.website + ' " target="_blank">' + marker.feature.properties.website + '</a></p>' ;
-                
-                marker.bindPopup(marker.feature.properties.map_label, {closeButton: false});
-                
-                //marker.bindLabel(marker.feature.properties.map_label, {className: "hovertip", direction: 'auto'})
-                
-                marker.on('mouseover', function() {
-                    marker.openPopup();
-                });
-                /*marker.on('mouseout', function() {
-                    marker.closePopup();
-                })*/;
-                
-                //// ADDING PLANT NAMES TO DROPDOWN LIST
-                
-                plantList = document.getElementById('plant-list').appendChild(document.createElement('li'));
-                plantList.className = 'droplist';
-                plantList.innerHTML = marker.feature.properties.plant_name;                
-                plantList.onclick = function() {
-                    marker.bindPopup(content);
-                    L.mapbox.tileLayer('elcurr.l4gdgnij').addTo(map);
-                    map.setView(marker.getLatLng(), 16);
-                    marker.openPopup();
-                };
-                
-                //// MARKER CLICK EVENT
-                marker.on('click', function(){
-                    marker.bindPopup(content);
-                    L.mapbox.tileLayer('elcurr.l4gdgnij').addTo(map);
-                    map.setView(marker.getLatLng(), 16);
-                    marker.openPopup();
-                    } );
-                
-                //// SETTING MARKER STYLES FOR OPERATING/PROPOSED
-                if (marker.feature.properties.status === 'Operating') {
-                    // The argument to L.mapbox.marker.icon is based on the
-                    // simplestyle-spec: see that specification for a full
-                    // description of options.
+                this.eachLayer(function(marker) {
+                    var content = '<p>Port</p>';
+                    marker.bindPopup(content, {closeButton: false});                     
                     marker.setIcon(L.mapbox.marker.icon({
-                        'marker-color': '#911815',
-                        'marker-size': 'small'
-                    }));
-                } else {
-                    marker.setIcon(L.mapbox.marker.icon({
-                        'marker-color': '#E85118',
-                        'marker-size': 'small'
-                    }));
-                }
-            })
+                            'marker-color': '#052945',
+                            'marker-size': 'small',
+                            'marker-symbol': 'ferry'
+                        }));
+                })
         })
-        
-        ////DRAWING FEATURES TO MAP
         .addTo(map);
-
+        
+        var label = document.createElement('label');
+        var description = document.createTextNode('Ports of Export');
+        var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox'
+            checkbox.className = 'active';
+            checkbox.innerHTML = '<i class="fa fa-square" style= "color:'+color+'></i> Ports of Export';
+            checkbox.checked = 'True'
+    
+        checkbox.onclick = function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+    
+            if (map.hasLayer(ports)) {
+                map.removeLayer(ports);
+                this.className = '';
+            } else {
+                map.addLayer(ports);
+                this.className = 'active';
+            }
+        };
+    
+        label.appendChild(checkbox);
+        label.appendChild(description);
+        layers.appendChild(label);
+}
     
 
-}
 
-function toggleLayers(){
-    console.log("toggling")
-    if (document.getElementById('operating').checked) {
-        facilitiesLayer= 'biomass_data/facilties_pellet_operating.geojson'
-    }
-    addFacilities()
-}
+
+
 
     
